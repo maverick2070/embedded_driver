16f514e7bce11bb69d2ce0284a4c339831d03df3
maverick2070 <m14775611609@163.com>
2025-03-09 01:12:14 +0800

    Delete .modules.order.cmd

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/maverick/Desktop/pro/code/embeded_driver/modules.order := {   echo /home/maverick/Desktop/pro/code/embeded_driver/firstdriver.o; :; } > /home/maverick/Desktop/pro/code/embeded_driver/modules.order
